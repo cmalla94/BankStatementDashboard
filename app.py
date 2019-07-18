a75bf7fa8e02ce3208b445f0392f3753af55b9a1
Chad <cmalla@sfu.com>
2019-07-18 12:41:02 -0700

    added empty layout to app.py to stop heroku error

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ import dash
 external_stylesheets = ['https://cdn.rawgit.com/plotly/dash-app-stylesheets/2d266c578d2a6e8850ebce48fdb52759b2aef506/stylesheet-oil-and-gas.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+layout = html.Div()
 server = app.server
 app.config['suppress_callback_exceptions']=True
