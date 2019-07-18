768aca5fc8692238cbe8c1fda2d190fc16d94784
Chad <cmalla@sfu.com>
2019-07-18 12:48:29 -0700

    problem is the procfile, had to switch app:server to index:server

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # main app file 
 
 import dash 
-import dash_html_components as html 
+
 
 external_stylesheets = ['https://cdn.rawgit.com/plotly/dash-app-stylesheets/2d266c578d2a6e8850ebce48fdb52759b2aef506/stylesheet-oil-and-gas.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-layout = html.Div()
+
 server = app.server
 app.config['suppress_callback_exceptions']=True
