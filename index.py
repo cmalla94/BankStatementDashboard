b4654b19dbc59c7a672d19e30398028fffd7ce30
Chad <cmalla@sfu.com>
2019-07-18 13:17:56 -0700

    added sentry for better debugging because heroku logs is not that useful with H10 error

--- index.py
+++ index.py
@@ -1,16 +1,24 @@
 # the index file of the application where the app navigates from
+# For debuggin and tracking errors
+import sentry_sdk
+sentry_sdk.init("https://90dd8cb534ad4e5e9a2f25dc27cc18fd@sentry.io/1508448")
+
+# test 
+# div_by_zero = 1 / 0
 
 # necessary imports
 import dash_core_components as dcc 
 import dash_html_components as html 
 from dash.dependencies import Input, Output
 
+
+
 # import the pages (apps) & import app
 from apps import budget_manager, hist_trans_tab
 from app import app
 
 app.layout = html.Div([
-    dcc.Tabs(
+    dcc.Tabs( 
         id='tabs',
         value = 'hist_trans_tab',
         children=[
