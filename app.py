475fa4d7d02a02f6a3cf2ad5059df7296fcc20f8
Chad <cmalla@sfu.com>
2019-07-18 12:44:11 -0700

    needed to add html to app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # main app file 
 
 import dash 
+import dash_html_components as html 
 
 external_stylesheets = ['https://cdn.rawgit.com/plotly/dash-app-stylesheets/2d266c578d2a6e8850ebce48fdb52759b2aef506/stylesheet-oil-and-gas.css']
 
